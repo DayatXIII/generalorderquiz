852ce64ee9b8b1da6bbde773670a993679456629
DayatXIII <dayat_qks@hotmail.com>
2019-02-13 18:52:03 +0800

    first commit

---
+++ src/jquery.questions.general.orders.js
@@ -0,0 +1,112 @@
+$('#quiz').quiz({
+    //resultsScreen: '#results-screen',
+    //counter: false,
+    //homeButton: '#custom-home',
+    counterFormat: 'Question %current of %total',
+    questions: [
+      {
+        'q': 'Peraturan-peraturan (Lantikan-Lantikan dan Kenaikan-Kenaikan Pangkat) Pegawai-Pegawai Kerajaan 1961 di kuatkuasakan pada bila?',
+        'options': [
+          '1 Januari 1962.',
+          '5 March 1961.',
+          '26 November 1970.',
+          '10 Oktober 1962.'
+        ],
+        'correctIndex': 0,
+        'correctResponse': 'Correct.',
+        'incorrectResponse': 'Incorrect. <br /> Answer: 1 Januari 1962'
+      },
+      {
+        'q': 'Ada kah boleh di ambil bekerja semula pegawai-pegawai yang telah berhenti?',
+        'options': [
+          'Tidak Boleh.',
+          'Boleh.'
+        ],
+        'correctIndex': 0,
+        'correctResponse': 'Correct.',
+        'incorrectResponse': 'Incorrect. <br /> Answer: Tiada sesiapun yang telah berhenti dari perkhidmatan awam di Negara Brunei Darussalam boleh di ambil bekerja semula di dalam mana-mana jawatan dengan tidak mendapat kebenaran yang tepat dari Suruhanjaya.'
+      },
+      {
+        'q': 'Cadangan-cadangan bagi lantikan-lantikan pemangku akan di selenggarakan mengikut perintah-perintah yang dikeluarkan oleh?',
+        'options': [
+          'Ketua Pejabat.',
+          'Suruhanjaya.',
+          'Pegawai Tertinggi.',
+          'Menteri Jabatan.'
+        ],
+        'correctIndex': 1,
+        'correctResponse': 'Correct.',
+        'incorrectResponse': 'Incorrect. <br /> Suruhanjaya.'
+      },
+      {
+        'q': 'Jika tarikh peranakan yang sebenar tidak di ketahui, maka tarikh diperanakkan hendaklah dianggap sebagai?',
+        'options': [
+          '1 Januari tahun itu.',
+          '1 Jun tahun itu.',
+          '31 Jun tahun itu.',
+          '31 Disember tahun itu.'
+        ],
+        'correctIndex': 3,
+        'correctResponse': 'Correct.',
+        'incorrectResponse': 'Incorrect <br /> 31 Disember tahun itu.'
+      },
+      {
+        'q': 'Ada kah boleh di ambil bekerja semula pegawai-pegawai yang telah dibuang kerja?',
+        'options': [
+          'Tidak boleh sama sekali.',
+          'Boleh, tetapi hanya dalam keadaan-keadaan khas dan berkecualian sahaja.'
+        ],
+        'correctIndex': 1,
+        'correctResponse': 'Correct',
+        'incorrectResponse': 'Incorrect <br /> Boleh, tetapi hanya dalam keadaan-keadaan khas dan berkecualian sahaja.'
+      },
+      {
+        'q': 'Kaedah pengiklanan apakah yang dibukakan kepada orang ramai yang berkelayakan (termasuk pegawai-pegawai kerajaan)?',
+        'options': [
+          'Bab 7(a)',
+          'Bab 38(a)',
+          'Bab 38(b)',
+          'Bab 7(b)'
+        ],
+        'correctIndex': 0,
+        'correctResponse': 'Correct.',
+        'incorrectResponse': 'Incorrect <br /> Bab 7(a).'
+      },
+      {
+        'q': 'Tiap-tiap pegawai apabila mula-mula dilantik ke satu jawatan, dikehendaki berkhidmat dalam percubaan selama berapa lama sebelum di tetapkan dalam jawatan?',
+        'options': [
+          '6 Bulan.',
+          '1 Tahun',
+          '3 Tahun',
+          '5 Tahun'
+        ],
+        'correctIndex': 2,
+        'correctResponse': 'Correct.',
+        'incorrectResponse': 'Incorrect <br /> Answer: 3 Tahun.'
+      },
+      {
+        'q': 'Berapa jam kah waktu kerja bagi seorang pegawai kerajaan yang sedang berkhidmat dalam perkhidmatan awam?',
+        'options': [
+          '7 1/2 Jam.',
+          '8 Jam.',
+          '7 Jam.',
+          '8 1/2 Jam.'
+        ],
+        'correctIndex': 0,
+        'correctResponse': 'Correct.',
+        'incorrectResponse': 'Incorrect <br /> Answer: 7 1/2 Jam.'
+      },
+      {
+        'q': 'Bila kah waktu pekerjaan bagi pegawai perkhidmatan awam?',
+        'options': [
+          'Dari jam 8.00 pagi hingga 12.30 tengahari dan 2.00 petang hingga 5.00 petang.',
+          'Dari jam 7.30 pagi hingga 12.30 tengahari dan 1.30 petang hingga 5.00 petang.',
+          'Dari jam 7.15 pagi hingga 12.00 tengahari dan 1.30 petang hingga 4.00 petang.',
+          'Dari jam 7.45 pagi hingga 12.15 tengahari dan 1.30 petang hingga 4.30 petang.'
+        ],
+        'correctIndex': 3,
+        'correctResponse': 'Correct.',
+        'incorrectResponse': 'Incorrect <br /> Answer: Dari jam 7.45 pagi hingga 12.15 tengahari dan 1.30 petang hingga 4.30 petang.'
+      }
+    ]
+  });
