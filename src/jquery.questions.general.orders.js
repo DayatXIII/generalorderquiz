57906d8bafa22b2f30fde66a455f945ba8ca313d
DayatXIII <dayat_qks@hotmail.com>
2020-06-18 18:39:53 +0800

    no message

--- src/jquery.questions.general.orders.js
+++ src/jquery.questions.general.orders.js
@@ -1,112 +1,344 @@
-$('#quiz').quiz({
-    //resultsScreen: '#results-screen',
-    //counter: false,
-    //homeButton: '#custom-home',
-    counterFormat: 'Soalan ke %current daripada %total',
-    questions: [
-      {
-        'q': 'Peraturan-peraturan (Lantikan-Lantikan dan Kenaikan-Kenaikan Pangkat) Pegawai-Pegawai Kerajaan 1961 di kuatkuasakan pada bila?',
-        'options': [
-          '1 Januari 1962.',
-          '5 March 1961.',
-          '26 November 1970.',
-          '10 Oktober 1962.'
-        ],
-        'correctIndex': 0,
-        'correctResponse': 'Betul.',
-        'incorrectResponse': 'Salah. <br /> Jawapan: 1 Januari 1962'
-      },
-      {
-        'q': 'Ada kah boleh di ambil bekerja semula pegawai-pegawai yang telah berhenti?',
-        'options': [
-          'Tidak Boleh.',
-          'Boleh.'
-        ],
-        'correctIndex': 0,
-        'correctResponse': 'Betul.',
-        'incorrectResponse': 'Salah. <br /> Jawapan: Tiada sesiapun yang telah berhenti dari perkhidmatan awam di Negara Brunei Darussalam boleh di ambil bekerja semula di dalam mana-mana jawatan dengan tidak mendapat kebenaran yang tepat dari Suruhanjaya.'
-      },
-      {
-        'q': 'Cadangan-cadangan bagi lantikan-lantikan pemangku akan di selenggarakan mengikut perintah-perintah yang dikeluarkan oleh?',
-        'options': [
-          'Ketua Pejabat.',
-          'Suruhanjaya.',
-          'Pegawai Tertinggi.',
-          'Menteri Jabatan.'
-        ],
-        'correctIndex': 1,
-        'correctResponse': 'Betul.',
-        'incorrectResponse': 'Salah. <br /> Jawapan: Suruhanjaya.'
-      },
-      {
-        'q': 'Jika tarikh peranakan yang sebenar tidak di ketahui, maka tarikh diperanakkan hendaklah dianggap sebagai?',
-        'options': [
-          '1 Januari tahun itu.',
-          '1 Jun tahun itu.',
-          '31 Jun tahun itu.',
-          '31 Disember tahun itu.'
-        ],
-        'correctIndex': 3,
-        'correctResponse': 'Betul.',
-        'incorrectResponse': 'Salah <br /> Jawapan: 31 Disember tahun itu.'
-      },
-      {
-        'q': 'Ada kah boleh di ambil bekerja semula pegawai-pegawai yang telah dibuang kerja?',
-        'options': [
-          'Tidak boleh sama sekali.',
-          'Boleh, tetapi hanya dalam keadaan-keadaan khas dan berkecualian sahaja.'
-        ],
-        'correctIndex': 1,
-        'correctResponse': 'Betul',
-        'incorrectResponse': 'Salah <br /> Jawapn: Boleh, tetapi hanya dalam keadaan-keadaan khas dan berkecualian sahaja.'
-      },
-      {
-        'q': 'Kaedah pengiklanan apakah yang dibukakan kepada orang ramai yang berkelayakan (termasuk pegawai-pegawai kerajaan)?',
-        'options': [
-          'Bab 7(a)',
-          'Bab 38(a)',
-          'Bab 38(b)',
-          'Bab 7(b)'
-        ],
-        'correctIndex': 0,
-        'correctResponse': 'Betul.',
-        'incorrectResponse': 'Salah <br /> Jawapan: Bab 7(a).'
-      },
-      {
-        'q': 'Tiap-tiap pegawai apabila mula-mula dilantik ke satu jawatan, dikehendaki berkhidmat dalam percubaan selama berapa lama sebelum di tetapkan dalam jawatan?',
-        'options': [
-          '6 Bulan.',
-          '1 Tahun',
-          '3 Tahun',
-          '5 Tahun'
-        ],
-        'correctIndex': 2,
-        'correctResponse': 'Betul.',
-        'incorrectResponse': 'Salah <br /> Jawapan: 3 Tahun.'
-      },
-      {
-        'q': 'Berapa jam kah waktu kerja bagi seorang pegawai kerajaan yang sedang berkhidmat dalam perkhidmatan awam?',
-        'options': [
-          '7 1/2 Jam.',
-          '8 Jam.',
-          '7 Jam.',
-          '8 1/2 Jam.'
-        ],
-        'correctIndex': 0,
-        'correctResponse': 'Betul.',
-        'incorrectResponse': 'Salah <br /> Jawapan: 7 1/2 Jam.'
-      },
-      {
-        'q': 'Bila kah waktu pekerjaan bagi pegawai perkhidmatan awam?',
-        'options': [
-          'Dari jam 8.00 pagi hingga 12.30 tengahari dan 2.00 petang hingga 5.00 petang.',
-          'Dari jam 7.30 pagi hingga 12.30 tengahari dan 1.30 petang hingga 5.00 petang.',
-          'Dari jam 7.15 pagi hingga 12.00 tengahari dan 1.30 petang hingga 4.00 petang.',
-          'Dari jam 7.45 pagi hingga 12.15 tengahari dan 1.30 petang hingga 4.30 petang.'
-        ],
-        'correctIndex': 3,
-        'correctResponse': 'Betul.',
-        'incorrectResponse': 'Salah <br /> Jawapan: Dari jam 7.45 pagi hingga 12.15 tengahari dan 1.30 petang hingga 4.30 petang.'
-      }
-    ]
-  });
+$("#quiz").quiz({
+  //resultsScreen: '#results-screen',
+  //counter: false,
+  //homeButton: '#custom-home',
+  counterFormat: "Soalan ke %current daripada %total",
+  questions: [
+    {
+      q:
+        "Peraturan-peraturan (Lantikan-Lantikan dan Kenaikan-Kenaikan Pangkat) Pegawai-Pegawai Kerajaan 1961 di kuatkuasakan pada bila?",
+      options: [
+        "1 Januari 1962.",
+        "5 March 1961.",
+        "26 November 1970.",
+        "10 Oktober 1962."
+      ],
+      correctIndex: 0,
+      correctResponse: "Betul.",
+      incorrectResponse: "Salah. <br /> Jawapan: 1 Januari 1962"
+    },
+    {
+      q:
+        "Ada kah boleh di ambil bekerja semula pegawai-pegawai yang telah berhenti?",
+      options: ["Tidak Boleh.", "Boleh."],
+      correctIndex: 0,
+      correctResponse: "Betul.",
+      incorrectResponse:
+        "Salah. <br /> Jawapan: Tiada sesiapun yang telah berhenti dari perkhidmatan awam di Negara Brunei Darussalam boleh di ambil bekerja semula di dalam mana-mana jawatan dengan tidak mendapat kebenaran yang tepat dari Suruhanjaya."
+    },
+    {
+      q:
+        "Cadangan-cadangan bagi lantikan-lantikan pemangku akan di selenggarakan mengikut perintah-perintah yang dikeluarkan oleh?",
+      options: [
+        "Ketua Pejabat.",
+        "Suruhanjaya.",
+        "Pegawai Tertinggi.",
+        "Menteri Jabatan."
+      ],
+      correctIndex: 1,
+      correctResponse: "Betul.",
+      incorrectResponse: "Salah. <br /> Jawapan: Suruhanjaya."
+    },
+    {
+      q:
+        "Jika tarikh peranakan yang sebenar tidak di ketahui, maka tarikh diperanakkan hendaklah dianggap sebagai?",
+      options: [
+        "1 Januari tahun itu.",
+        "1 Jun tahun itu.",
+        "31 Jun tahun itu.",
+        "31 Disember tahun itu."
+      ],
+      correctIndex: 3,
+      correctResponse: "Betul.",
+      incorrectResponse: "Salah <br /> Jawapan: 31 Disember tahun itu."
+    },
+    {
+      q:
+        "Ada kah boleh di ambil bekerja semula pegawai-pegawai yang telah dibuang kerja?",
+      options: [
+        "Tidak boleh sama sekali.",
+        "Boleh, tetapi hanya dalam keadaan-keadaan khas dan berkecualian sahaja."
+      ],
+      correctIndex: 1,
+      correctResponse: "Betul",
+      incorrectResponse:
+        "Salah <br /> Jawapn: Boleh, tetapi hanya dalam keadaan-keadaan khas dan berkecualian sahaja."
+    },
+    {
+      q:
+        "Kaedah pengiklanan apakah yang dibukakan kepada pegawai-pegawai yang sedang berkhidmat di kementerian atau jabatan itu sendiri?",
+      options: ["Bab 7(a)", "Bab 38(a)", "Bab 38(b)", "Bab 7(b)"],
+      correctIndex: 2,
+      correctResponse: "Betul.",
+      incorrectResponse: "Salah <br /> Jawapan: Bab 38(b)."
+    },
+    {
+      q:
+        "Kaedah pengiklanan apakah yang dibukakan kepada pegawai-pegawai yang sedang berkhidmat di kementerian-kementerian dan jabatan-jabatan kerajaan?",
+      options: ["Bab 7(a)", "Bab 38(a)", "Bab 38(b)", "Bab 7(b)"],
+      correctIndex: 1,
+      correctResponse: "Betul.",
+      incorrectResponse: "Salah <br /> Jawapan: Bab 38(a)."
+    },
+    {
+      q:
+        "Kaedah pengiklanan apakah yang dibukakan kepada orang ramai yang berkelayakan (termasuk pegawai-pegawai kerajaan)?",
+      options: ["Bab 7(a)", "Bab 38(a)", "Bab 38(b)", "Bab 7(b)"],
+      correctIndex: 0,
+      correctResponse: "Betul.",
+      incorrectResponse: "Salah <br /> Jawapan: Bab 7(a)."
+    },
+    {
+      q:
+        "Tiap-tiap pegawai apabila mula-mula dilantik ke satu jawatan, dikehendaki berkhidmat dalam percubaan selama berapa lama sebelum di tetapkan dalam jawatan?",
+      options: ["6 Bulan.", "1 Tahun", "3 Tahun", "5 Tahun"],
+      correctIndex: 2,
+      correctResponse: "Betul.",
+      incorrectResponse: "Salah <br /> Jawapan: 3 Tahun."
+    },
+    {
+      q:
+        "Berapa jam kah waktu kerja bagi seorang pegawai kerajaan yang sedang berkhidmat dalam perkhidmatan awam?",
+      options: ["7 1/2 Jam.", "8 Jam.", "7 Jam.", "8 1/2 Jam."],
+      correctIndex: 0,
+      correctResponse: "Betul.",
+      incorrectResponse: "Salah <br /> Jawapan: 7 1/2 Jam."
+    },
+    {
+      q: "Bila kah waktu pekerjaan bagi pegawai perkhidmatan awam?",
+      options: [
+        "Dari jam 8.00 pagi hingga 12.30 tengahari dan 2.00 petang hingga 5.00 petang.",
+        "Dari jam 7.30 pagi hingga 12.30 tengahari dan 1.30 petang hingga 5.00 petang.",
+        "Dari jam 7.15 pagi hingga 12.00 tengahari dan 1.30 petang hingga 4.00 petang.",
+        "Dari jam 7.45 pagi hingga 12.15 tengahari dan 1.30 petang hingga 4.30 petang."
+      ],
+      correctIndex: 3,
+      correctResponse: "Betul.",
+      incorrectResponse:
+        "Salah <br /> Jawapan: Dari jam 7.45 pagi hingga 12.15 tengahari dan 1.30 petang hingga 4.30 petang."
+    },
+    {
+      q: "Siapa kah tokoh adat istiadat Brunei?",
+      options: [
+        "Sultan Omar Ali Saifuddin III",
+        "Sultan Ahmad Tajuddin",
+        "Sultan Bokiah",
+        "Sultan Omar Ali Saifuddin II"
+      ],
+      correctIndex: 0,
+      correctResponse: "Betul.",
+      incorrectResponse: "Salah <br /> Jawapan: Sultan Omar Ali Saifuddin III."
+    },
+    {
+      q: "Bila kah Sultan Omar Ali Saifuddin turun dari takhta?",
+      options: [
+        "5 April 1986",
+        "5 Januari 1967",
+        "5 Oktober 1967",
+        "15 Mei 1965"
+      ],
+      correctIndex: 2,
+      correctResponse: "Betul.",
+      incorrectResponse: "Salah <br /> Jawapan: 15 Oktober 1967."
+    },
+    {
+      q: "Bila kah Jabatan Tenaga di naik tarafkan?",
+      options: [
+        "10 Januari 2018",
+        "10 March 2018",
+        "18 Julai 2018",
+        "18 April 2018"
+      ],
+      correctIndex: 3,
+      correctResponse: "Betul.",
+      incorrectResponse: "Salah <br /> Jawapan: 18 April 2018."
+    },
+    {
+      q:
+        "Apakah borang yang hendak di isi untuk memohon kebenaran memiliki syarikat?",
+      options: ["Borang 14/15", "Borang 15/16", "Borang 16/17", "Borang 17/18"],
+      correctIndex: 2,
+      correctResponse: "Betul.",
+      incorrectResponse: "Salah <br /> Jawapan: Borang 16/17."
+    },
+    {
+      q:
+        "Berapa kah jenis-jenis kad pengenalan yang dikeluarkan oleh Jabatan Immigresen dan Kebangsaan?",
+      options: ["1 jenis", "2 jenis", "3 jenis", "4 jenis"],
+      correctIndex: 2,
+      correctResponse: "Betul.",
+      incorrectResponse: "Salah <br /> Jawapan: 3 jenis."
+    },
+    {
+      q:
+        "Apakah gelaran yang diberikan kepada ahli-ahli majlis mesyuarat negara?",
+      options: [
+        "Yang Bmat Berhormat",
+        "Yang Berhormat",
+        "Tuan / Puan",
+        "Yang Dipertuan"
+      ],
+      correctIndex: 1,
+      correctResponse: "Betul.",
+      incorrectResponse: "Salah <br /> Jawapan: Yang Berhormat."
+    },
+    {
+      q: "Bila kah pertukaran nama Bandar Brunei menjadi Bandar Seri Begawan?",
+      options: [
+        "4 Oktober 1970",
+        "4 September 1970",
+        "10 Oktober 1965",
+        "10 September 1965"
+      ],
+      correctIndex: 0,
+      correctResponse: "Betul.",
+      incorrectResponse: "Salah <br /> Jawapan: 4 Oktober 1970."
+    },
+    {
+      q:
+        "Siapa kah mengisytiharkan pertukaran Bandar Brunei menjadi Bandar Seri Begawan?",
+      options: [
+        "Dato Seri Paduka Dr. Awang Haji Mohd. Amin Liew bin Abdullah",
+        "Pehin Datu Lailaraja Majer Jeneral (B) Dato Paduka Seri Haji Awang Halbi bin Haji Mohd. Yusof",
+        "Pengiran Dato Seri Paduka Haji Bahrom bin Pengiran Haji Bahar",
+        "Pengiran Setia Negara Pengiran Haji Mohd Yusof Pengiran Haji Abdul Rahim"
+      ],
+      correctIndex: 3,
+      correctResponse: "Betul.",
+      incorrectResponse:
+        "Salah <br /> Jawapan: Pengiran Setia Negara Pengiran Haji Mohd Yusof Pengiran Haji Abdul Rahim."
+    },
+    {
+      q: "Apakah maksud bulan sabit yang terletak pada bendera Brunei?",
+      options: [
+        "Lambang keadilan, kedamaian, kemakmuran dan keamanan",
+        "Lambang Kerajaan di bawah seorang raja",
+        "Lambang Islam"
+      ],
+      correctIndex: 2,
+      correctResponse: "Betul.",
+      incorrectResponse: "Salah <br /> Jawapan: Lambang Islam."
+    },
+    {
+      q: "Apakah maksud payung obor-obor yang terletak pada bendera Brunei?",
+      options: [
+        "Lambang keadilan, kedamaian, kemakmuran dan keamanan",
+        "Lambang Kerajaan di bawah seorang raja",
+        "Lambang Islam"
+      ],
+      correctIndex: 1,
+      correctResponse: "Betul.",
+      incorrectResponse:
+        "Salah <br /> Jawapan: Lambang Kerajaan di bawah seorang raja."
+    },
+    {
+      q: "Apakah maksud sayap yang terletak pada bendera Brunei?",
+      options: [
+        "Lambang keadilan, kedamaian, kemakmuran dan keamanan",
+        "Lambang Kerajaan di bawah seorang raja",
+        "Lambang Islam"
+      ],
+      correctIndex: 0,
+      correctResponse: "Betul.",
+      incorrectResponse:
+        "Salah <br /> Jawapan: Lambang keadilan, kedamaian, kemakmuran dan keamanan."
+    },
+    {
+      q: "Apakah yang tertulis pada bulan sabit bendera Brunei",
+      options: [
+        "Sentiasa berbuat kebajikan dengan petunjuk Allah",
+        "Negara Brunei Darussalam",
+        "Allah Peliharakan Sultan"
+      ],
+      correctIndex: 0,
+      correctResponse: "Betul.",
+      incorrectResponse:
+        "Salah <br /> Jawapan: Sentiasa berbuat kebajikan dengan petunjuk Allah."
+    },
+    {
+      q: "Apakah yang tertulis di <b>bawah</b> bulan sabit pada bendera Brunei",
+      options: [
+        "Sentiasa berbuat kebajikan dengan petunjuk Allah",
+        "Negara Brunei Darussalam",
+        "Allah Peliharakan Sultan"
+      ],
+      correctIndex: 1,
+      correctResponse: "Betul.",
+      incorrectResponse: "Salah <br /> Jawapan: Negara Brunei Darussalam."
+    },
+    {
+      q: "Apakah yang di maksudkan dengan visi dan misi?",
+      options: [
+        "Visi adalah impian yang hendak dicapai dan misi adalah tujuan",
+        "Visi adalah matlamat dan misi adalah pelan",
+        "Visi adalah gambaran besar dan misi adalah langkah-langkah yang dilakukan untuk mencapai visi tersebut"
+      ],
+      correctIndex: 2,
+      correctResponse: "Betul.",
+      incorrectResponse:
+        "Salah <br /> Jawapan: Visi adalah gambaran besar dan misi adalah langkah-langkah yang dilakukan untuk mencapai visi tersebut."
+    },
+    {
+      q: "Bilakah tahun kewangan Brunei?",
+      options: [
+        "1 hb Mac sehigga 31 hb Febuari",
+        "1 hb April sehigga 31 hb Mac",
+        "1 hb Mei sehigga 31 hb April",
+        "1 hb June sehigga 31 hb Mei"
+      ],
+      correctIndex: 1,
+      correctResponse: "Betul.",
+      incorrectResponse: "Salah <br /> Jawapan: 1 hb April sehigga 31 hb Mac."
+    },
+    {
+      q: "Apakah gelaran terasul bagi Pengiran Muda Mahkota?",
+      options: [
+        "Duli Yang Teramat Mulia Paduka Seri Pengiran Muda Mahkota Pengiran Muda Haji Al-Muhtadee Billah bin Kebawah Duli Yang Maha Mulia Paduka Seri Baginda Sultan Haji Hassanal Bolkiah Mu'izzaddin Waddaulah.",
+        "Duli Yang Teramat Mulia Paduka Seri Pengiran Muda Mahkota Pengiran Muda Haji Al-Muhtadee Billah bin Kebawah Duli Yang Maha Mulia Paduka Seri Baginda Sultan Haji Hassanal Bolkiah.",
+        "Duli Yang Teramat Mulia Paduka Seri Pengiran Muda Mahkota Pengiran Muda (Dr.) Haji Al-Muhtadee Billah",
+        "Duli Yang Teramat Mulia Paduka Seri Pengiran Muda Mahkota Pengiran Muda (Dr.) Haji Al-Muhtadee Billah ibni Kebawah Duli Yang Maha Mulia Paduka Seri Baginda Sultan Haji Hassanal Bolkiah Mu'izzaddin Waddaulah."
+      ],
+      correctIndex: 3,
+      correctResponse: "Betul.",
+      incorrectResponse:
+        "Salah <br /> Jawapan: Duli Yang Teramat Mulia Paduka Seri Pengiran Muda Mahkota Pengiran Muda (Dr.) Haji Al-Muhtadee Billah ibni Kebawah Duli Yang Maha Mulia Paduka Seri Baginda Sultan Haji Hassanal Bolkiah Mu'izzaddin Waddaulah."
+    },
+    {
+      q: "Apakah gelaran terasul bagi KDYMM Sultan?",
+      options: [
+        "Kebawah Duli Yang Maha Mulia Paduka Seri Baginda Sultan Haji Hassanal Bolkiah Mu'izzaddin Waddaulah ibni Al-Marhum Sultan Haji Omar 'Ali Saifuddien Sa'adul Khairi Waddien, Sultan dan Yang Di-Pertuan Negara Brunei Darussalam",
+        "Kebawah Duli Yang Maha Mulia Paduka Seri Baginda Sultan Haji Hassanal Bolkiah Mu'izzaddin Waddaulah ibni Al-Marhum Sultan Haji Omar 'Ali Saifuddien, Sultan dan Yang Di-Pertuan Negara Brunei Darussalam.",
+        "Kebawah Duli Yang Maha Mulia Paduka Seri Baginda Sultan Haji Hassanal Bolkiah Mu'izzaddin Waddaulah bin Al-Marhum Sultan Haji Omar 'Ali Saifuddien Sa'adul Khairi Waddien, Sultan dan Yang Di-Pertuan Negara Brunei Darussalam",
+        "Kebawah Duli Yang Maha Mulia Paduka Seri Baginda Sultan Haji Hassanal Bolkiah Mu'izzaddin Waddaulah bin Al-Marhum Sultan Haji Omar 'Ali Saifuddien, Sultan dan Yang Di-Pertuan Negara Brunei Darussalam."
+      ],
+      correctIndex: 0,
+      correctResponse: "Betul.",
+      incorrectResponse:
+        "Salah <br /> Jawapan: Kebawah Duli Yang Maha Mulia Paduka Seri Baginda Sultan Haji Hassanal Bolkiah Mu'izzaddin Waddaulah ibni Al-Marhum Sultan Haji Omar 'Ali Saifuddien Sa'adul Khairi Waddien, Sultan dan Yang Di-Pertuan Negara Brunei Darussalam."
+    },
+    {
+      q: "Apakah yang dimaksudkan dengan Fail Meja?",
+      options: [
+        "Fail Meja adalah fail yang berisi informasi tentang sesuatu perkara yang perlu diketahui oleh seseorang pegawai",
+        "Fail Meja adalah fail yang mengandungi peraturan-peraturan pegawai-pegawai kerajaan (kelakuan dan tatatertib)",
+        "Fail Meja adalah fail rujukan-rujukan yang mengandungi maklumat atau panduan untuk menjalankan tugas bagi satu-satu jawatan"
+      ],
+      correctIndex: 2,
+      correctResponse: "Betul.",
+      incorrectResponse:
+        "Salah <br /> Jawapan: Fail Meja adalah fail rujukan-rujukan yang mengandungi maklumat atau panduan untuk menjalankan tugas bagi satu-satu jawatan."
+    },
+    {
+      q: "Apakah faedah KKC (Kumpulan Kerja Cemerlang)?",
+      options: [
+        "Memupuk kerjasama di kalangan pekerja dan menggalakkan kreativiti",
+        "Pengumpulan data, analisis dan pembentangan",
+        "Penapisan senarai (short-listing)"
+      ],
+      correctIndex: 0,
+      correctResponse: "Betul.",
+      incorrectResponse:
+        "Salah <br /> Jawapan: Memupuk kerjasama di kalangan pekerja dan menggalakkan kreativiti."
+    }
+  ]
+});
